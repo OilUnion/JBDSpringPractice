708b7292c404314744da62c33a61939a200c9b02
Konstantin Albatov <albatovkonstantin@yandex.ru>
2025-11-13 20:17:06 +0300

    :pencil: add controller + MVC

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
 
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.14")
     implementation("io.swagger.core.v3:swagger-annotations:2.2.16")
 
     implementation("org.springframework.boot:spring-boot-starter-actuator")
